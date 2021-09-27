13b2d73eb534881af1ac2078a17e581d87cea0f3
leonardoDallos <leo3.14@hotmail.com>
2021-09-27 17:28:37 -0500

    añadido envio de informacion vía mail de contacto app

--- Web_proyect/Web_proyect/settings.py
+++ Web_proyect/Web_proyect/settings.py
@@ -131,3 +131,13 @@ MEDIA_ROOT=BASE_DIR / 'media' # añadido para acceder a contenido mutimedia guar
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#campos para el uso de email en backend
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'dalloshernando@gmail.com'
+EMAIL_HOST_PASSWORD = 'blackfire365'
+
+
--- Web_proyect/contacto/views.py
+++ Web_proyect/contacto/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from contacto.forms import Formulario_contacto
+from django.core.mail import send_mail, EmailMessage #clases para enviar un email.
 
 # Create your views here.
 def contacto(request):
@@ -12,6 +13,19 @@ def contacto(request):
             email = request.POST.get('email')
             contenido = request.POST.get('contenido')
 
-            return redirect('/contacto/?valido')
+            email = EmailMessage('Mensaje enviado desde app django_web_store', f'El usuario con nombre {nombre}, con la dirección {email} escribe lo siguiente: \n\n {contenido}','', ["leo3.14@hotmail.com"], reply_to=[email]) # reply_to=email -> direccion a donde se pueda contestar este correo enviado
+            #email = EmailMessage("Mensaje enviado desde app django_web_store", "El usuario con nombre  con la dirección  escribe lo siguiente: \n\n ","dalloshernando@gmail.com", ["leo3.14@hotmail.com"], reply_to=[email]) # reply_to=email -> direccion a donde se pueda contestar este correo enviado
+            
+            #send_mail('Mensaje enviado desde app django_web_store', "El usuario con nombre  con la dirección  escribe lo siguiente: \n\n ", "", ["leo3.14@hotmail.com"])
+            
+            #email.send()
+            try:
+                 email.send()
+                 return redirect('/contacto/?valido')
+
+            except:
+                 return redirect('/contacto/?novalido')
+
+            
 
     return render(request, 'contacto.html', {'formulario_contacto':formulario_contacto})
