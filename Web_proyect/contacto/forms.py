8208bceb93d926b6bf8ab11d65c24f308554097f
leonardoDallos <leo3.14@hotmail.com>
2021-09-27 16:23:45 -0500

    formulario de app contacto actualizado

--- Web_proyect/contacto/forms.py
+++ Web_proyect/contacto/forms.py
@@ -3,4 +3,4 @@ from django import forms
 class Formulario_contacto(forms.Form):
     nombre = forms.CharField(label='Nombre', required=True)
     email = forms.CharField(label='E-mail', required=True)
-    contenido = forms.CharField(label='Contenido')
+    contenido = forms.CharField(label='Contenido', widget=forms.Textarea) # el parametro widget=forms.Textarea se usa para la renderizcion de este campo de formulario
--- Web_proyect/contacto/views.py
+++ Web_proyect/contacto/views.py
@@ -1,8 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from contacto.forms import Formulario_contacto
 
 # Create your views here.
 def contacto(request):
     formulario_contacto = Formulario_contacto()
+
+    if request.method == 'POST':
+        formulario_contacto = Formulario_contacto(data=request.POST)
+        if formulario_contacto.is_valid():
+            nombre = request.POST.get('nombre')
+            email = request.POST.get('email')
+            contenido = request.POST.get('contenido')
+
+            return redirect('/contacto/?valido')
 
     return render(request, 'contacto.html', {'formulario_contacto':formulario_contacto})
