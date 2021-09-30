5478a6db94e2967443dd1463a62771652fd4e73c
leonardoDallos <leo3.14@hotmail.com>
2021-09-30 17:01:58 -0500

    pagina tienda que visualiza productos agregada

--- Web_proyect/tienda/admin.py
+++ Web_proyect/tienda/admin.py
@@ -3,13 +3,13 @@ from tienda import models
 
 class CategoriaProdAdmin(admin.ModelAdmin):
     #list_display = ('nombre')
-    #search_fields = ('nombre')
+    search_fields = ('nombre', 'created', 'updated')
     readonly_fields = ('created', 'updated')
 
 class ProductoAdmin(admin.ModelAdmin):
     list_display = ('nombre', 'categoria', 'imagen', 'precio', 'disponibilidad')
     search_fields = ('nombre', 'categoria', 'imagen', 'precio', 'disponibilidad')
-   # readonly_fields = ('created', 'updated')
+    readonly_fields = ('created', 'updated')
 
 
 # Register your models here.
--- Web_proyect/tienda/views.py
+++ Web_proyect/tienda/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .models import CategoriaProd, Producto
 
 # Create your views here.
 def tienda(request):
 
-    return render(request, 'tienda.html')
+    productos = Producto.objects.all() # importando todo los productos guardados en la DDBB.
+    return render(request, 'tienda.html', {'productos':productos})
